72201e723972d9afa20596033ffc29ee9e603ca6
Anton Veijola <antonv1997@gmail.com>
2025-02-14 19:52:57 +0200

    add validations to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
+  validates :title, :body, presence: true
+  validates :title, :body, length: { minimum: 5 }
+
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   validates :username, presence: { message: "You need to have username" }
   validates :username, length: { minimum: 3 }
   validates :username, uniqueness: { message: "Username is already taken" }
+
+  has_many :posts
 end
