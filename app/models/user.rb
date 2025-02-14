40e09e2dd9cfe223a9127661dca8148928b28d61
Anton Veijola <antonv1997@gmail.com>
2025-02-14 19:38:02 +0200

    add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, presence: { message: "You need to have username" }
+  validates :username, length: { minimum: 3 }
+  validates :username, uniqueness: { message: "Username is already taken" }
 end
